bc8dcb02a18f2bf79e217b997868168d68b692a1
makaimark <makaimark@gmail.com>
2016-07-27 11:18:29 +0200

    Story 1: fill database with examples

--- example_data.py
+++ example_data.py
@@ -1,3 +1,16 @@
 # This script can generate example data for "City" and "InterviewSlot" models.
 
 from models import *
+
+
+'''codecool_budapest = School.create(location = 'Budapest', name = 'Codecool_Budapest')
+codecool_miskolc = School.create(location = 'Miskolc', name = 'Codecool_Miskolc')
+codecool_krakow = School.create(location = 'Krakow', name = 'Codecool_Krakow')
+
+cities = [{'name': 'Budapest', 'school': codecool_budapest}, {'name': 'Lillafüred', 'school': codecool_miskolc},
+          {'name': 'Balatonlelle', 'school': codecool_budapest}, {'name': 'Warsav', 'school': codecool_krakow},
+          {'name': 'Pusztaszentjakab', 'school': codecool_miskolc}, {'name': 'Sopron', 'school': codecool_budapest}]
+
+for city in cities:
+    City.create(name=city['name'], school=city['school'])'''
+
