86238454d90e65d59d419d816e361d9c9be0f793
makaimark <makaimark@gmail.com>
2016-07-27 10:46:53 +0200

    models are ready

--- models.py
+++ models.py
@@ -1,12 +1,30 @@
+
 from peewee import *
 
 # Configure your database connection here
 # database name = should be your username on your laptop
 # database user = should be your username on your laptop
-db = PostgresqlDatabase('dbname', user='dbuser')
+db = PostgresqlDatabase('school_system_project', user='makaimark')
 
 
 class BaseModel(Model):
     """A base model that will use our Postgresql database"""
     class Meta:
         database = db
+
+class School(BaseModel):
+    location = CharField()
+    name = CharField()
+
+class City(BaseModel):
+    name = CharField()
+    school = ForeignKeyField(School, related_name='city_of_school')
+
+class Applicant(BaseModel):
+    application_code = CharField(unique=True)
+    first_name = CharField()
+    last_name = CharField()
+    gender = CharField()
+    email_address= CharField(unique=True)
+    city = ForeignKeyField(City, related_name='city_of_applicant')
+    status = CharField(default='New')
