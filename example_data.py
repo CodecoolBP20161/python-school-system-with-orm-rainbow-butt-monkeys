9072ea3ffdbf8521be7ad84ea0f07eea12f0aaad
Shevah92 <shevah92@gmail.com>
2016-07-28 11:05:00 +0200

    Story 2: interview slot add to applicant

--- example_data.py
+++ example_data.py
@@ -2,7 +2,7 @@
 
 from models import *
 
-
+'''
 codecool_budapest = School.create(location = 'Budapest', name = 'Codecool_Budapest')
 codecool_miskolc = School.create(location = 'Miskolc', name = 'Codecool_Miskolc')
 codecool_krakow = School.create(location = 'Krakow', name = 'Codecool_Krakow')
@@ -12,8 +12,7 @@ cities = [{'name': 'Budapest', 'school': codecool_budapest}, {'name': 'Lillafür
           {'name': 'Pusztaszentjakab', 'school': codecool_miskolc}, {'name': 'Sopron', 'school': codecool_budapest}]
 
 for city in cities:
-    City.create(name=city['name'], school=city['school'])
-
+    City.create(name=city['name'], school=city['school'])'''
 
 
 
@@ -21,6 +20,7 @@ for city in cities:
 
 
 '''
+
 codecool_bp = School.select().where(School.location == "Budapest")
 codecool_miskolc = School.select().where(School.location == "Miskolc")
 codecool_krakow = School.select().where(School.location == "Krakow")
@@ -28,19 +28,20 @@ codecool_krakow = School.select().where(School.location == "Krakow")
 mentors = [{'first_name': 'Miki', 'last_name': 'Beöthy', 'school': codecool_bp},
            {'first_name': 'Tomi', 'last_name': 'Tompa', 'school': codecool_bp},
            {'first_name': 'Dani', 'last_name': 'Salamon', 'school': codecool_bp},
-           {'first_name': 'Mateus', 'last_name': 'Grabowski', 'school': codecool_krakow}]
-
+           {'first_name': 'Mateus', 'last_name': 'Grabowski', 'school': codecool_krakow}]'''
+'''
 for mentor in mentors:
-    Mentor.create(**mentor)
-'''
-'''
+    Mentor.create(**mentor)'''
+
+
 mentors = Mentor.select()
 interview_slots = [{'mentor': mentors[0], 'start': '2016-08-01 12:00:00', 'end': '2016-08-01 13:00:00', 'is_reserved': False},
                    {'mentor': mentors[1], 'start': '2016-08-01 14:00:00', 'end': '2016-08-01 15:00:00','is_reserved': False},
                    {'mentor': mentors[2], 'start': '2016-08-01 12:00:00', 'end': '2016-08-01 13:00:00','is_reserved': False},
-                   {'mentor': mentors[3], 'start': '2016-08-03 12:00:00', 'end': '2016-08-03 13:00:00','is_reserved': False}]
+                   {'mentor': mentors[2], 'start': '2016-08-04 12:00:00', 'end': '2016-08-04 13:00:00','is_reserved': False},
+                    {'mentor': mentors[1], 'start': '2016-08-07 12:00:00', 'end': '2016-08-07 13:00:00','is_reserved': False}]
 
 for slot in interview_slots:
     InterviewSlot.create(**slot)
-    '''
+
 
