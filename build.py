12d02acfd6331c92e747223b096b1d1927af2c17
Kincses Dani <danikincs@gmail.com>
2016-07-28 13:55:54 +0200

    Write comments everywhere, pep-8 compatible

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from models import *
 db.connect()
 # List the tables here what you want to create...
 
-print("Drop tables")
+print("Drop tables")  # Drop all tables automatic.
 db.drop_tables([Applicant, School, City, Mentor, Interview, InterviewSlot], safe=True)
-print("Create tables")
+print("Create tables")  # Create all tables automatic.
 db.create_tables([Applicant, School, City, Mentor, Interview, InterviewSlot], safe=True)
