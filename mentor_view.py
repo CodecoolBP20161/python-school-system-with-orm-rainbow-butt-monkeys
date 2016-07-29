cef02ec5a4f8a1896278a7873e5957455fba7ae3
Kincses Dániel <danikincs@gmail.com>
2016-07-29 11:25:13 +0200

    Delete mentor_view.py
    functions are now in the main class

--- mentor_view.py
+++
@@ -1,7 +0,0 @@
-from models import *
-
-def interview_details(mentor_id):
-    interview_query = Interview.select(Interview, Applicant).join(Applicant).where(Interview.mentor==mentor_id)
-    for interview in interview_query:
-        print ("\nDate of interview: ", interview.date, "\nName of applicant: ",interview.applicant.first_name, "",
-               interview.applicant.last_name, "\nApplication code: ", interview.applicant.application_code)
