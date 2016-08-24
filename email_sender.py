71e07dd956cb328bee58909f58918e286ec9e495
Kincses Dani <danikincs@gmail.com>
2016-08-24 14:40:13 +0200

    cleaning the code for pep8, delete unused rows, add comments

--- email_sender.py
+++ email_sender.py
@@ -2,16 +2,15 @@ import smtplib
 import config
 
 
-class SendEmail():
+class SendEmail:  # Main class for e-mail sending with every obligatory attributes.
 
     def __init__(self, email_to, subject, message):
         self.email_to = email_to
         self.subject = subject
         self.message = message
 
-    @staticmethod
+    @staticmethod  # Main logic with password, sender e-mail, and server infos.
     def email_sender(to, subject, text):
-        # Gmail Sign In
         gmail_sender = config.email_address
         gmail_passwd = config.email_password
 
@@ -24,34 +23,38 @@ class SendEmail():
                             'From: %s' % gmail_sender,
                             'Subject: %s' % subject,
                             '', text])
-
         try:
             server.sendmail(gmail_sender, [to], BODY)
-            print(text)
+            print("E-mail send to: %s" % to)
         except:
-            print("béna vagy")
-
+            print("Can't send e-mail.")
         server.quit()
 
 
 def send_email(applicant_email, applicant_first_name, application_code, school_location):
+    # E-mail sending to applicant about his/her application code.
     to = applicant_email
     subject = 'Your Application'
-    text ='\r\n'.join(['Dear %s!' % applicant_first_name, 'Your application code is %d.' % application_code, 'You have to attend your interview in: %s' % school_location])
+    text ='\r\n'.join(['Dear %s!' % applicant_first_name, 'Your application code is %d.' % application_code,
+                       'You have to attend your interview in: %s' % school_location])
     SendEmail.email_sender(to, subject, text)
 
 
 def send_email_for_interview(applicant_email, applicant_first_name, mentor_name, interview_start):
+    # E-mail sending to applicant about his/her mentor, and interview slot.
     to = applicant_email
     subject = 'Your Application'
-    text ='\r\n'.join(['Dear %s!' % applicant_first_name, 'Your mentor will be %s.' % mentor_name, 'You have to attend your interview at: %s' % interview_start])
+    text ='\r\n'.join(['Dear %s!' % applicant_first_name, 'Your mentor will be %s.' % mentor_name,
+                       'You have to attend your interview at: %s' % interview_start])
     SendEmail.email_sender(to, subject, text)
 
 
 def send_email_to_mentor(mentor_email, mentor_first_name, applicant_first_name, reserved_interview_slot):
+    # E-mail sending to mentor about his reserved interview slot, and applicant name.
     to = mentor_email
     subject = 'Your Interview Slots'
-    text ='\r\n'.join(['Dear %s!' % mentor_first_name, 'You will have an interview at %s.' % reserved_interview_slot, 'You will have an interview with: %s' % applicant_first_name])
+    text ='\r\n'.join(['Dear %s!' % mentor_first_name, 'You will have an interview at %s.' % reserved_interview_slot,
+                       'You will have an interview with: %s' % applicant_first_name])
     SendEmail.email_sender(to, subject, text)
 
 
