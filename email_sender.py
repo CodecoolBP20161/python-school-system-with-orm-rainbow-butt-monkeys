9cd1d58b68bc9f05bf64518394aeffcf416e9f78
Kincses Dani <danikincs@gmail.com>
2016-08-24 11:23:21 +0200

    email sender for mentor started

--- email_sender.py
+++ email_sender.py
@@ -55,3 +55,32 @@ def send_email_for_interview(applicant_email, applicant_first_name, mentor_name,
         print(applicant_email)
 
     server.quit()
+
+def send_email_to_mentor(mentor_email, mentor_first_name, applicant_first_name, reserved_interview_slot):
+    TO = mentor_email
+    SUBJECT = 'Your Interview Slots'
+    TEXT ='\r\n'.join(['Dear %s!' % mentor_first_name, 'You will have an interview at %s.' % reserved_interview_slot, 'You will have an interview with: %s' % applicant_first_name])
+
+    # Gmail Sign In
+    gmail_sender = config.email_address
+    gmail_passwd = config.email_password
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_sender, gmail_passwd)
+
+    BODY = '\r\n'.join(['To: %s' % TO,
+                        'From: %s' % gmail_sender,
+                        'Subject: %s' % SUBJECT,
+                        '', TEXT])
+
+    try:
+        server.sendmail(gmail_sender, [TO], BODY)
+        print('email sent')
+    except:
+        print(applicant_email)
+
+    server.quit()
+
+send_email_to_mentor('danikincs@gmail.com', 'Miki', 'Dani', '1222.00.11')
