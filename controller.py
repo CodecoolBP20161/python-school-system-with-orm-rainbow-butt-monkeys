c1d43afdd5259ae768da0a7b939e7a845e024e5e
DorciM <dora.medgyasszay@gmail.com>
2016-09-06 13:54:06 +0200

    filtering logic in progress

--- controller.py
+++ controller.py
@@ -29,6 +29,30 @@ def get_applicant():
     return redirect(config.address)
 
 
+@app.route('/admin', methods=['GET'])
+def empty_filter():
+    return render_template('filter_menu.html')
+
+@app.route('/admin', methods=['POST'])
+def list_applicants():
+    option = request.form['Filter By']
+    filter = request.form['filter']
+    if option == 'Status':
+        Applicant.filter_status(filter)
+    elif option == 'Time':
+        Applicant.filter_reg_time(filter)
+    elif option == 'Location':
+        Applicant.filter_location(filter)
+    elif option == 'Name':
+        Applicant.filter_name(filter)
+    elif option == 'Email':
+        Applicant.filter_email(filter)
+    elif option == 'School':
+        Applicant.filter_school(filter)
+    elif option == 'Mentor name':
+        Applicant.filter_mentor(filter)
+    else:
+        return 'Not working'
 
 
 if __name__ == '__main__':
