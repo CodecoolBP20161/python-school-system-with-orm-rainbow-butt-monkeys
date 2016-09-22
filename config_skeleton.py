8ef02da2ad6e91ef940f177f95b50158084010d7
makaimark <makaimark@gmail.com>
2016-09-22 10:27:10 +0200

    admin log in works for the first time

--- config_skeleton.py
+++
@@ -1,7 +0,0 @@
-name = ""
-dbname = "school_system_project"
-host = "localhost"
-password = ""
-email_address = "rbm.codecool@gmail.com"
-email_password = "rbm2016codecool"
-address = "http://localhost:5000"
